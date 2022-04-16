7cb3b1366492efdeb3e3de59eb6443306a23ae00
memo <mehmetbaki.123@gmail.com>
2022-04-16 22:58:23 +0300

    ok

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+//ok
+function saturdayFun(params="roller-skate") {
+    
+    return `This Saturday, I want to ${params}!`
+}
+
+
+function mondayWork(params='go to the office') {
+    return `This Monday, I will ${params}.`}
+
+
+
+function wrapAdjective(params = "*"){
+        const innerF = function (param="special"){
+            return `You are ${params}${param}${params}!`
+        }
+    
+        return innerF;
+}
+
+wrapAdjective("!!!")
